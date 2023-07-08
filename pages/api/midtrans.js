768cd98b88235a1e8fc453b2f76e7b0b41a9dedb
Crazy-Newbie <ezie2001@gmail.com>
2023-07-08 16:21:59 +0700

    setup midtrans

---
+++ pages/api/midtrans.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  const midtransClient = require("midtrans-client");
+  // Create Snap API instance
+  const isProd = process.env.NODE_ENV === "production";
+  let snap = new midtransClient.Snap({
+    // Set to true if you want Production Environment (accept real transaction).
+    isProduction: isProd,
+    serverKey: process.env.NEXT_PUBLIC_SERVER_KEY_MIDTRANS,
+  });
+  let snapToken;
+  const data = req.body;
+  console.log(data, "MIDTRANS DATA IN API ROUTE");
+  try {
+    await snap.createTransaction(data).then((transaction) => {
+      // transaction token
+      snapToken = transaction.token;
+      console.log("transactionToken:", snapToken);
+    });
+    if (req.method === "POST") {
+      res.status(200).json({ snapToken });
+    }
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+}
