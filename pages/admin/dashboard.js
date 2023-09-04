4e8ac19bb764e8e218ebd00b68d7024dfe34980d
Crazy-Newbie <ezie2001@gmail.com>
2023-09-05 00:49:07 +0700

    debug

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -79,7 +79,10 @@ export default function Dashboard({ orderservice, service }) {
         },
       }) => {
         return (
-          <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+          <tr
+            key={id}
+            class="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+          >
             <th
               scope="row"
               class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
